ebb9acce5f842fb213282b5da5b27a52d3b5d404
Michael Houmann <mhoumann87@gmail.com>
2023-04-13 19:11:08 +0200

    Install react-routs and react-routes-dom, create Routes in App.js and create empty pages for the routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import ForgotPassword from './pages/ForgotPassword';
+import SignUp from './pages/SignUp';
+
 function App() {
   return (
     <>
-      <h1>Upstart</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+      </Router>
     </>
   );
 }
